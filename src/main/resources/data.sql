b800147f18211658a6bfddba8d715ee35a6cc5f4
Enrique Pineda <132867213+EnriqueWCCI@users.noreply.github.com>
2023-08-17 19:52:07 -0400

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,6 +2,3 @@
 DROP TABLE IF EXISTS customer CASCADE;
 CREATE TABLE customer(id INT, first_name VARCHAR(255), last_name VARCHAR(255), date_joined VARCHAR(255));
 
--- executed only if seeding is enabled in application.properties 
-INSERT INTO customer VALUES(1, 'firstName1', 'lastName1', '12 August 2023');
-INSERT INTO customer VALUES(2, 'firstName2', 'lastName2', '12 August 2023');
